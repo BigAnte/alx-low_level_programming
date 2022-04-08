19b2f6667262cd1d5cdca64ee15e34f322cef3af
frank4wealth <frankonojaifecareer@gmail.com>
2022-04-07 20:15:17 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,36 +1,23 @@
-nclude <stdio.h>
+#include <stdio.h>
+
+
 
 /**
  *
+ *  * main - prints the phrase with proper grammar,
  *
+ *   * but the outcome is a piece of art,
  *
- * * main - Function begins
+ *    * Return: 0 if exited properly, non-zero otherwise
  *
- *
- *
- * *
- *
- *
- *
- * * Return: Function ends
- *
- *
- *
- * */
-
-
+ *     */
 
 int main(void)
 
-
-
 {
 
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
+			return (0);
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-
-
-	return (0)
 }
