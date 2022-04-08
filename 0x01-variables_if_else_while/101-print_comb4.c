3bf6a1a7190f4447497289f30d927383d86aaa06
frank4wealth <frankonojaifecareer@gmail.com>
2022-04-08 14:49:02 -0700

    task 10 commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,10 +5,10 @@
  * Description:  prints all possible different combinations of three digits.
  * Return 0
  */
+
 int main(void)
 {
 int digit1, digit2,digit3;
-
 for (digit1 = 0; digit1 < 8; digit1++)
 {
 for (digit2 = digit1 + 1; digit2 < 9; digit2++)
@@ -21,7 +21,6 @@ putchar((digit3 % 10) + '0');
 if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 continue;
 putchar(','); 
-
 putchar(' ');
 }
 }
