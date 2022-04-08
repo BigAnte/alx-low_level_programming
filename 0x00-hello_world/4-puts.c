2ef737ce98057fe4027fc52f005aa300fe7da023
frank4wealth <frankonojaifecareer@gmail.com>
2022-04-07 19:25:21 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+
+
 /**
- *  *main - function begins
  *
- *     *Return: function ends
- *      */
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
 
 int main(void)
 
