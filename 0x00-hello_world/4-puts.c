bb3a0c07383ef56cc4e20334afaaab25d8890c8e
frank4wealth <frankonojaifecareer@gmail.com>
2022-04-09 09:17:54 -0700

    mtask 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
 /**
 *main - function begins
- 
-*Return: function ends
- */
-
+*
+*Return: Always 0 (succes)
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-
-
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
