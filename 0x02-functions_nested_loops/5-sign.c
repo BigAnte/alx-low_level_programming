567a1b7c4dd81bfb7699fcd7913144acf0a669d4
frank4wealth <frankonojaifecareer@gmail.com>
2022-04-13 07:59:33 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,16 +6,13 @@
  *0 if the number is zero,
  *-1 if the number is less than zero.
  */
-
 int print_sign(int n)
 {
 if (n > 0) 
 {
 _putchar('+');
 return (1);
-}
 else if (n == 0)
-{
 _putchar('0');
 return (0);
 }
