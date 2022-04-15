979bfcb526b7f677d1e99cb501c437ea0e861626
frank4wealth <frankonojaifecareer@gmail.com>
2022-04-14 19:47:20 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - draws a square
+ * @size: length and width of square
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+
+int hgt, widt;
+if (size > 0)
+{
+for (hgt = 0; hgt < size; hgt++)
+{
+for (widt = 0; widt < size; widt++)
+_putchar('#')
+if (hgt == size - 1)
+continue;
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
