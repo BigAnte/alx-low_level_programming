959cb7bd88ff91c4d16b3f8de738c7807660f885
frank4wealth <frankonojaifecareer@gmail.com>
2022-04-24 15:40:42 -0700

    task 10

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,45 +1,34 @@
 #include <stdio.h>
 
-
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
 {
+	int n, m, l;
 
-	  int n;
-
-	    int a[5];
-
-	      int *p;
-
-
-
-	        a[2] = 1024;
-
-		  p = &n;
-
-		    /*
-		     *
-		     *    * write your line of code here...
-		     *
-		     *       * Remember:
-		     *
-		     *          * - you are not allowed to use a
-		     *
-		     *             * - you are not allowed to modify p
-		     *
-		     *                * - only one statement
-		     *
-		     *                   * - you are not allowed to code anything else than this line of code
-		     *
-		     *                      */
-
-		    p[5] = 98;
-
-		      /* ...so that this prints 98\n */
-
-		      printf("a[2] = %d\n", a[2]);
-
-		        return (0);
-
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
