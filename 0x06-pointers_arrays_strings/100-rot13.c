ea4f90abe5024655c34eab439cbc89820b33dfd6
frank4wealth <frankonojaifecareer@gmail.com>
2022-04-24 01:07:17 -0700

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,23 +8,25 @@
 
 char *rot13(char *str)
 {
-	int i;
+	int i, j;
 
-	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	char *ptr = s;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (*s)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		for (i = 0; i <= 52; i++)
+		j = 0;
+		while (alpha[j] != '\0')
 		{
-			if (*s == rot13[i])
+			if (str[i] == alpha[j])
 			{
-				*s = ROT13[i];
+				str[i] = rot[j];
 				break;
 			}
+			j++;
 		}
-		s++;
+		i++;
 	}
-	return (prt);
+	return (str);
 }
