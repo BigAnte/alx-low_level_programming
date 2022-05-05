942661577c34cfb0730801d68aed6c7fc434312a
frank4wealth <frankonojaifecareer@gmail.com>
2022-05-05 12:34:05 -0700

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -52,7 +52,8 @@ char **strtow(char *str)
 				aout[i] = malloc((c - a1 + 2) * sizeof(char));
 				if (aout[i] == NULL)
 				{
-					ch_free_grid(aout, i);						return (NULL);
+					ch_free_grid(aout, i);						
+					return (NULL);
 				}
 				break;
 			}
