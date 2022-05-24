03271b176c2e2e5adc578a7af98c0515aca32d81
frank4wealth <frank4wealth@DESKTOP-T2DIPHK.localdomain>
2022-05-24 21:02:08 +0100

    header_files

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * struct list_s - singly linked list
+ * struct list_s - singly linked lists
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
