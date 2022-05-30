a6bf6fdfc4a00f38cfdc376dbef64b44ab3b7c92
frank4wealth <frank4wealth@DESKTOP-T2DIPHK.localdomain>
2022-05-30 19:10:06 +0100

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
- *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- * Otherwise - the converted number.
+ * binary_to_uint - binary to unsigned int
+ * @b: param
+ * Return: unsigned int
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int ans = 0;
+	int base_len, base_two;
 
-	if (b == '\0')
-		return (0);
-
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	if (!b)
+		return (0);for
+	(base_len = 0; b[base_len] != '\0'; base_len++)
+		;
+	for (base_len--, base_two = 1; base_len >= 0; base_len--, base_two *= 2)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[base_len] != '0' && b[base_len] != '1')
+		{
 			return (0);
-
-		num += (b[len] - '0') * mult;
-
-		mult *= 2;
+		}
+		if (b[base_len] & 1)
+		{
+			ans += base_two;
+		}
 	}
-	return (num);
+	return (ans);
 }
+
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_
+#define MAIN_H_
 
 #include <stdlib.h>
+#include <stdio.h>
 
 
 int _putchar(char c);
