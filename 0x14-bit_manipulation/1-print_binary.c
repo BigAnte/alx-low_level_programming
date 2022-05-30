50f5a25b8d4173f9f7cef3c1f8ba171faf8fbaa5
frank4wealth <frank4wealth@DESKTOP-T2DIPHK.localdomain>
2022-05-30 19:32:17 +0100

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
- * rint_binary - converts unsigned int to binary
+/**
+ * print_binary - converts unsigned int to binary
  * @n: unsigned int
  * Return: binary
  */
