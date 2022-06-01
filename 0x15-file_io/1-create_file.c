9c0ddfeea9547e6547b2ff07672764ba62ec98df
frank4wealth <frank4wealth@DESKTOP-T2DIPHK.localdomain>
2022-06-01 16:22:29 +0100

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,6 @@ int create_file(const char *filename, char *text_content)
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (!text_content) /* if no text to write, just create the file */
-
 	{
 		close(o);
 		return (1);
@@ -35,7 +34,7 @@ int create_file(const char *filename, char *text_content)
 		close(o);
 		return (-1);
 	}
-
 	close(o);
+
 	return (1);
 }
