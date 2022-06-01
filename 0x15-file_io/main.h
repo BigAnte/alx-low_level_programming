3fd5c0056756fd2ef07cbaf1914587d58db526b0
frank4wealth <frank4wealth@DESKTOP-T2DIPHK.localdomain>
2022-06-01 15:54:19 +0100

    Header files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,11 +8,16 @@
 #include <fcntl.h>
 
 /* close */
+#include <unistd.h>
+
+/* dprintf */
 #include <stdio.h>
 
 /* malloc, free */
 #include <stdlib.h>
 
+
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
