1378b215232918f78a294c8e7e2414d64f3eedf4
frank4wealth <frank4wealth@DESKTOP-T2DIPHK.localdomain>
2022-06-05 19:25:29 +0100

    Task 11

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,93 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the times table
- * @n: integer for which the times table will be printed
- *
- * Description: prints the times table
- * Return: void
+ * long_value - print numbers greater than  9 with putchar
+ * @val: the number
+ * Return: nothing
  */
+void long_value(int val)
+{
+	if (val / 10)
+		long_value(val / 10);
+	_putchar(val % 10 + '0');
+}
 
+/**
+ * space - required space
+ * Return: nothing
+ */
+void space(void)
+{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(' ');
+}
+
+/**
+ * less_than_100 - if the count is greater than or equal 100
+ * Return: nothing
+ */
+void less_than_100(void)
+{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+}
+
+/**
+ * more_than_100 - count is more than 100
+ * Return: nothing
+ */
+void more_than_100(void)
+{
+	_putchar(',');
+	_putchar(' ');
+}
+/**
+ * print_times_table - print nth times table
+ * @n: the number of times table to be printed
+ * Return: nothing
+ */
 void print_times_table(int n)
 {
-int row, column, product;
-if (n >= 0 && n < 15)
-{
-for (row = 0; row <= n; row++)
-{
-for (column = 0; column <= n; column++)
-{
-product = row * column;
-if (column == 0)
-_putchar('0');
-else if (product < 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(product % 10 + '0');
-}
-else if (product >= 10 && product < 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar((product / 10) % 10 + '0');
-_putchar(product % 10 + '0');
-}
-else if (product > 99 && product < 1000)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(product / 100 + '0');
-_putchar((product / 10) % 10 + '0');
-_putchar(product % 10 + '0');
-}
-}
-_putchar('\n');
-}
-}
+	int i, j, count;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			if (i != n)
+				space();
+		}
+		for (i = 0; i <= n; i++)
+		{
+			for (count = 0; count <= n; count++)
+			{
+				j = i;
+				j *= count;
+				if (j == 0 && i >= 1)
+				{
+					_putchar('0');
+				}
+				if (j > 0 && j <= 9)
+				{
+					space();
+					_putchar(j + '0');
+				}
+				if (j > 9 && j < 100)
+				{
+					less_than_100();
+					long_value(j);
+				}
+				if (j >= 100)
+				{
+					more_than_100();
+					long_value(j);
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
